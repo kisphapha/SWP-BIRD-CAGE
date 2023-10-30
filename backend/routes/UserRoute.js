20b8bb4c1e2f74f5913a02145a1b053714cbceeb
Quang-Phat203 <quangphat7a1@gmail.com>
2023-10-30 17:47:34 +0700

    update point in user

--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -6,6 +6,6 @@ userRouter.get("/", UserController.getAllUser);
 userRouter.post("/new", UserController.newUser);
 userRouter.get("/update", UserController.updateUser);
 userRouter.get("/:email", UserController.getUserByEmail);
-
+userRouter.post("/updatePoint", UserController.getPointForUser);
 
 module.exports = userRouter;
