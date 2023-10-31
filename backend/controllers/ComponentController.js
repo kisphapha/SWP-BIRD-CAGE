4ba26af4e8b02da21ccdc26b82d09564b4f79be6
bomismegg <minhtri28102003@gmail.com>
2023-10-31 11:05:19 +0700

    update custom page

--- backend/controllers/ComponentController.js
+++ backend/controllers/ComponentController.js
@@ -2,12 +2,17 @@ const component = require("../models/Component");
 
 const getAllComponent = async (req, res) => {
     try {
-        const Component = await component.getAllComponent();
+        const CateID = req.query.CateID;
+        // res.json(Type);
+        const Component = await component.getAllComponent(CateID);
+        
         res.json(Component);
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
 }
+
+
 
 module.exports = {
     getAllComponent
--- frontend/src/Pages/Custom/index.jsx
+++ frontend/src/Pages/Custom/index.jsx
@@ -1,45 +1,73 @@
-﻿/* eslint-disable react/jsx-key */
-import { React, useState, useEffect } from 'react'
-import CategoryNav from '../../components/features/CategoryNav'
-import MenuItem from '@mui/material/MenuItem'
-import { UserProvider } from '../../UserContext'
-import Header from '../../components/common/Header'
-import Navbar from '../../components/common/Navbar'
-import { Button, Grid, RadioGroup, TextField, ToggleButtonGroup } from '@mui/material'
-import axios from 'axios'
+﻿import React, { useState, useEffect } from 'react';
+import CategoryNav from '../../components/features/CategoryNav';
+import MenuItem from '@mui/material/MenuItem';
+import { UserProvider } from '../../UserContext';
+import Header from '../../components/common/Header';
+import Navbar from '../../components/common/Navbar';
+import { Button, TextField } from '@mui/material';
+import axios from 'axios';
+
 export default function Custom() {
-    const [categories, setCategories] = useState([])
-    const [tmpName, setTempName] = useState('')
-    const [tmpMaterial, setMaterial] = useState('')
-    const [tmpCate, setCate] = useState('')
-    const [tmpDescription, setTempDescription] = useState('')
-    const [selectedImage, setSelectedImage] = useState(null)
+    const [categories, setCategories] = useState([]);
+    const [components, setComponents] = useState([]);
+    const [tmpName, setTempName] = useState('');
+    const [tmpMaterial, setMaterial] = useState('');
+    const [tmpCate, setCate] = useState('');
+    const [tmpDescription, setTempDescription] = useState('');
+    const [selectedImage, setSelectedImage] = useState(null);
+    const [componentType, setComponentType] = useState('');
+    const [selectedComponent, setSelectedComponent] = useState(null);
 
     const handleCategoryChange = (event) => {
-        setCategories(event.target.value.trim())
-    }
+        setCate(event.target.value);
+    };
+
     const handleNameChange = (event) => {
-        setTempName(event.target.value)
-    }
+        setTempName(event.target.value);
+    };
+
     const handleMaterialChange = (event) => {
-        setMaterial(event.target.value)
-    }
+        setMaterial(event.target.value);
+    };
+
     const handleDescriptionChange = (event) => {
-        setTempDescription(event.target.value)
-    }
-    //fetch
+        setTempDescription(event.target.value);
+    };
+
+    const handleComponentTypeChange = (event) => {
+        setComponentType(event.target.value);
+    };
+
+    const handleSelectedComponentChange = (event) => {
+        const selectedComponentData = components.find((component) => component.ID === event.target.value);
+        setSelectedComponent(selectedComponentData);
+    };
+
     async function fetchCategories() {
-        const response = await axios.get('http://localhost:3000/category/')
+        const response = await axios.get('http://localhost:3000/category/');
         if (response.data) {
-            setCategories(response.data)
+            setCategories(response.data);
         }
     }
-    useEffect(() => {
-        fetchCategories()
-    }, [])
 
-    const defaultImageClass = 'h-64 w-64 m-2 transition-transform transform-gpu rounded-lg'
-    const selectedImageClass = 'h-52<w-52></w-52> <w-52></w-52> m-2 hover:scale-105 border-2 border-blue-500'
+    async function fetchComponents(categoryId) {
+        const response = await axios.get('http://localhost:3000/component/getAllComponent', {
+            params: {
+                "CateID": categoryId 
+            }
+        });
+        if (response.data) {
+            setComponents(response.data);
+        }
+    }
+
+    useEffect(() => {
+        fetchCategories();
+    }, []);
+
+    const defaultImageClass = 'h-64 w-64 m-2 transition-transform transform-gpu rounded-lg';
+    const selectedImageClass = 'h-52 w-52 m-2 hover:scale-105 border-2 border-blue-500';
+
 
     return (
         <form action="">
@@ -48,90 +76,153 @@ export default function Custom() {
                     <Header />
                     <Navbar />
                 </UserProvider>
-                <CategoryNav parents={[{ name: 'Trang chủ', link: '/' }]} current="Lồng tùy chỉnh"></CategoryNav>
+                <CategoryNav parents={[{ name: 'Trang chủ', link: '/' }]} current="Lồng tùy chỉnh" />
                 <div className="flex-row bg-slate-50 my-8 mx-32">
                     <div className="px-8 py-4 font-bold text-red-500"> Chọn kiểu lồng</div>
                     <div className="grid grid-cols-4 place-items-center h">
                         {categories.map((category, index) => {
-                            if (category.Allow_customize == true) {
+                            if (category.Allow_customize === true) {
                                 return (
                                     <div
                                         className={selectedImage === index + 1 ? selectedImageClass : defaultImageClass}
-                                        onClick={() => setSelectedImage(index + 1)}
+                                        onClick={() => {
+                                            fetchComponents(category.id.trim())
+                                            setSelectedImage(index + 1)
+                                        }}
+                                        key={index}
                                     >
                                         <img className="max-h-52" src={category.imageUrl} alt="" />
                                         <div className="text-center">
                                             <h1 className="font-bold my-2">{category.name}</h1>
                                         </div>
                                     </div>
-                                )
+                                );
                             }
                         })}
                     </div>
                     <div className="flex my-12 place-content-between m-8">
                         <div className="w-2/5 mx-4 pr-20 flex-row space-y-4 pb-8 mb-16">
                             <div className="w-3/4">
-                                {/* <div>name</div> */}
-                                <TextField fullWidth label={'Tên sản phẩm'} variant="standard" onChange={handleNameChange} value={tmpName} />
+                                <TextField fullWidth label="Tên sản phẩm" variant="standard" onChange={handleNameChange} value={tmpName} />
                             </div>
-
-                            <TextField fullWidth select label="Móc" helperText="Chọn móc" variant="filled" onChange={handleCategoryChange}>
-                                {categories.map((option) => (
-                                    <MenuItem key={option.id} value={option.id}>
-                                        {option.name}
-                                    </MenuItem>
-                                ))}
-                            </TextField>
-                            <TextField fullWidth select label="Khung" helperText="Chọn khung" variant="filled" onChange={handleCategoryChange}>
-                                {categories.map((option) => (
-                                    <MenuItem key={option.id} value={option.id}>
-                                        {option.name}
-                                    </MenuItem>
-                                ))}
-                            </TextField>
-                            <TextField fullWidth select label="Nan" helperText="Chọn nan" variant="filled" onChange={handleCategoryChange}>
-                                {categories.map((option) => (
-                                    <MenuItem key={option.id} value={option.id}>
-                                        {option.name}
-                                    </MenuItem>
-                                ))}
-                            </TextField>
-                            <TextField fullWidth select label="Nắp" helperText="Chọn nắp" variant="filled" onChange={handleCategoryChange}>
-                                {categories.map((option) => (
-                                    <MenuItem key={option.id} value={option.id}>
-                                        {option.name}
-                                    </MenuItem>
-                                ))}
+                            <TextField
+                                fullWidth
+                                select
+                                label="Móc"
+                                helperText="Chọn móc"
+                                variant="filled"
+                                onChange={handleSelectedComponentChange}
+                            >
+                                {components.map((component) => {
+                                    if (component.Type === "Móc") {
+                                        return (
+                                            <MenuItem key={component.ID} value={component.ID}>
+                                                {component.Name}
+                                            </MenuItem>
+                                        )
+                                    }
+                                })}
                             </TextField>
                             <TextField
                                 fullWidth
                                 select
-                                label="Bình nước"
-                                helperText="Chọn bình nước"
+                                label="Khung"
+                                helperText="Chọn khung"
                                 variant="filled"
-                                onChange={handleCategoryChange}
+                                onChange={handleSelectedComponentChange}
                             >
-                                {categories.map((option) => (
-                                    <MenuItem key={option.id} value={option.id}>
-                                        {option.name}
-                                    </MenuItem>
-                                ))}
+                                {components.map((component) => {
+                                    if (component.Type === "Khung") {
+                                        return (
+                                            <MenuItem key={component.ID} value={component.ID}>
+                                                {component.Name}
+                                            </MenuItem>
+                                        )
+                                    }
+                                })}
                             </TextField>
-                            <TextField fullWidth select label="Đáy" helperText="Chọn đáy " variant="filled" onChange={handleCategoryChange}>
-                                {categories.map((option) => (
-                                    <MenuItem key={option.id} value={option.id}>
-                                        {option.name}
-                                    </MenuItem>
-                                ))}
+                            <TextField
+                                fullWidth
+                                select
+                                label="Nan"
+                                helperText="Chọn nan"
+                                variant="filled"
+                                onChange={handleSelectedComponentChange}
+                            >
+                                {components.map((component) => {
+                                    if (component.Type === "Nan") {
+                                        return (
+                                            <MenuItem key={component.ID} value={component.ID}>
+                                                {component.Name}
+                                            </MenuItem>
+                                        )
+                                    }
+                                })}
+                            </TextField>
+                            <TextField
+                                fullWidth
+                                select
+                                label="Nắp"
+                                helperText="Chọn nắp"
+                                variant="filled"
+                                onChange={handleSelectedComponentChange}
+                            >
+                                {components.map((component) => {
+                                    if (component.Type === "Nắp") {
+                                        return (
+                                            <MenuItem key={component.ID} value={component.ID}>
+                                                {component.Name}
+                                            </MenuItem>
+                                        )
+                                    }
+                                })}
+                            </TextField>
+                            <TextField
+                                fullWidth
+                                select
+                                label="Đáy"
+                                helperText="Chọn đáy "
+                                variant="filled"
+                                onChange={handleSelectedComponentChange}
+                            >
+                                {components.map((component) => {
+                                    if (component.Type === "Đáy") {
+                                        return (
+                                            <MenuItem key={component.ID} value={component.ID}>
+                                                {component.Name}
+                                            </MenuItem>
+                                        )
+                                    }
+                                })}
                             </TextField>
                         </div>
-                        <div className="w-2/5 mr-40 ">
-                            <div className=" my-4 mt-12">
+                        <div className="w-2/5 mr-40">
+                            <div className="my-4 mt-12">
                                 <TextField
                                     fullWidth
                                     select
                                     label="Bình nước"
                                     helperText="Chọn bình nước"
+                                    variant="filled"
+                                    onChange={handleSelectedComponentChange}
+                                >
+                                    {components.map((component) => {
+                                        if (component.Type === "Bình nước") {
+                                            return (
+                                                <MenuItem key={component.ID} value={component.ID}>
+                                                    {component.Name}
+                                                </MenuItem>
+                                            )
+                                        }
+                                    })}
+                                </TextField>
+                            </div>
+                            <div className="my-4 w-full">
+                                <TextField
+                                    fullWidth
+                                    select
+                                    label="Size"
+                                    helperText="Kích thước"
                                     variant="filled"
                                     onChange={handleCategoryChange}
                                 >
@@ -141,50 +232,20 @@ export default function Custom() {
                                         </MenuItem>
                                     ))}
                                 </TextField>
-                            </div>
-                            <div className=" my-4 w-full">
-                                <TextField fullWidth select label="Size" helperText="Kích thước " variant="filled" onChange={handleCategoryChange}>
-                                    {categories.map((option) => (
-                                        <MenuItem key={option.id} value={option.id}>
-                                            {option.name}
-                                        </MenuItem>
-                                    ))}
-                                </TextField>
                                 <div className="flex w-full">
                                     <div className="w-20 h-20">
-                                        <img src="https://mengjinblog.files.wordpress.com/2021/06/17.jpg" alt="" />
+                                        <img src={selectedComponent?.Image || 'https://mengjinblog.files.wordpress.com/2021/06/17.jpg'} alt="" />
                                     </div>
                                     <div className="w-full ml-4">
-                                        <div>Mô tả : ádasdasdasdsadasdsadasdas</div>
+                                        <div>Mô tả: {selectedComponent?.Description || 'No description available'}</div>
                                         <div className="flex place-content-between">
-                                            <div>Thời gian chế tạo: </div>
-                                            <div> Giá tiền: 3000</div>
+                                            <div>Thời gian chế tạo: {selectedComponent?.ProductionTime || 'N/A'}</div>
+                                            <div>Giá tiền: {selectedComponent?.Price || 'N/A'}</div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                            <div className=" my-4 w-full">
-                                <TextField fullWidth select label="Size" helperText="Kích thước " variant="filled" onChange={handleCategoryChange}>
-                                    {categories.map((option) => (
-                                        <MenuItem key={option.id} value={option.id}>
-                                            {option.name}
-                                        </MenuItem>
-                                    ))}
-                                </TextField>
-                                <div className="flex w-full">
-                                    {/* <div className="w-20 h-20">
-                                        <img src="https://mengjinblog.files.wordpress.com/2021/06/17.jpg" alt="" />
-                                    </div> */}
-                                    <div className="w-full ml-4">
-                                        <div>Mô tả :Phù hợp chim ........</div>
-                                        <div className="flex place-content-between">
-                                            <div>40x60x80 </div>
-                                            <div>Thời gian chế tạo: </div>
-                                            <div> Giá tiền: 3000</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+
                             <TextField
                                 fullWidth
                                 label={'Mô tả'}
@@ -204,8 +265,9 @@ export default function Custom() {
                             </div>
                         </div>
                     </div>
+
                 </div>
             </div>
         </form>
-    )
+    );
 }
