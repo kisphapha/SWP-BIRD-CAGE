4d7f37e9ddd86e8efaa869a5e136ff2344e3904c
eleonora-vil <vinhthitheokaka@gmail.com>
2023-11-03 11:39:35 +0700

    add

--- frontend/src/Pages/ShipperPage/index.jsx
+++ frontend/src/Pages/ShipperPage/index.jsx
@@ -72,6 +72,7 @@ export default function Shipper() {
                     ordersWithItems.push(orderWithItems)
                 }
             }
+            // sdasd
             setCards(ordersWithItems)
         }
     }
